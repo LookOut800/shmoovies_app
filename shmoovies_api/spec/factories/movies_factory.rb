71578be8655fba2d35fe4ff07ce2163356e51280
J-Weeks <weeksjasons@gmail.com>
2015-03-05 20:40:20 -0500

    added movie factories

--- shmoovies_api/spec/factories/movies_factory.rb
+++ shmoovies_api/spec/factories/movies_factory.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :movie do
-    title Faker::Lorem.sentence
-    body Faker::Lorem.paragraphs(3)
-    author Faker::Name.name
+    title Faker::Commerce.product_name
+    release_date Faker::Date.backward(1820)
+    mpaa_rating Faker::Address.state_abbr
+    description Faker::Company.bs
   end
 end
