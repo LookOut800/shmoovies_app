b1bd2380133a0e999e163efbccfd6b70d8c0beb6
Max Blaushild <blaushmild@gmail.com>
2015-03-06 18:20:44 -0500

    moviesArray fills up, but not able to deferred call of handlebars until after all ajax requests have completed

--- shmoovies_front_end/app/js/tmdb.js
+++ shmoovies_front_end/app/js/tmdb.js
@@ -1,17 +1,15 @@
-
 var movieArray = [];
 
 var Tmdb = Tmdb || {
   tUrl: 'http://api.themoviedb.org/3/movie/',
   key: '?api_key=c688559a6fc49e17efa91cf8b357837f',
-  rUrl: 'http://localhost:3000',
-  slider: []
+  rUrl: 'http://localhost:3000'
 };
 
 $(document).ready(function(){
   Tmdb.getMoviesFromRails();
 });
-/*
+
 Tmdb.getMovie = function(movie){
   $.ajax({
     url: Tmdb.tUrl + movie.tmbd_id + Tmdb.key,
@@ -23,43 +21,48 @@ Tmdb.getMovie = function(movie){
     console.log(jqXHR, textStatus, errorThrown);
   });
 };
-*/
 
 Tmdb.moviesIndex = function(data){
-  data.forEach(function(currentVal,index,array){
-    $.ajax({
-        url: Tmdb.tUrl + currentVal.tmbd_id + Tmdb.key,
-        type: 'GET',
-        success:function(data){
-          console.log(data);
-          movieArray << data;
-        }
-      }).done(function(data){
-        console.log(data);
-        // movieArray << data;
-      })
-      .fail(function(jqXHR, textStatus, errorThrown){
-        console.log(jqXHR, textStatus, errorThrown);
-      });
-  });
-  // Tmdb.renderSlider(Tmdb.slider);
+  $.when(
+
+    $.each(function(currentVal,index,array){
+      $.ajax({
+          url: Tmdb.tUrl + currentVal.tmbd_id + Tmdb.key,
+          type: 'GET',
+          success: function(data){
+            movieArray.push(data);
+            console.log(movieArray)
+          }
+        })
+      // .done(function(data){
+        //   movieArray.push(data);
+        //   console.log(movieArray);
+        // })
+        // .fail(function(jqXHR, textStatus, errorThrown){
+        //   console.log(jqXHR, textStatus, errorThrown);
+        // });
+    })
+
+  ).done(function(data){
+    console.log(movieArray);
+  })
 }
 
 
-/*Tmdb.renderShow = function(data){
-  // var template = Handlebars.compile($('#movie-show').html());
-  // $('#content').html(template({
-  //   movie: data
-  // }));
-};*/
-/*
+Tmdb.renderShow = function(data){
+  var template = Handlebars.compile($('#movie-show').html());
+  $('#content').html(template({
+    movie: data
+  }));
+};
+
 Tmdb.renderSlider = function(data){
   var template = Handlebars.compile($('#movie-show').html());
   $('#content').html(template({
     movies: data
   }));
 };
-*/
+
 Tmdb.getMoviesFromRails = function(){
   $.ajax({
     url: Tmdb.rUrl + '/movies',
@@ -72,22 +75,19 @@ Tmdb.getMoviesFromRails = function(){
     console.log(jqXHR, textStatus, errorThrown);
   });
 };
-/*
-Tmdb.moviesDirect =function(){
 
-}
-*/// Tmdb.submitMovie = function(){
-//   if(event.preventDefault) event.preventDefault();
-//   $.ajax({
-//     url: Tmdb.rUrl + '/movies',
-//     type: 'POST',
-//     data: { movie: {
-//               XXXXXXXXXXXXXXXXXXXXXXXXX,
-//          }
-//       }
-//   }).done(function(data){
-//     trace(data);
-//   }).fail(function(jqXHR, textStatus, errorThrown){
-//     trace(jqXHR, textStatus, errorThrown);
-//   });
-// };
+Tmdb.submitMovie = function(){
+  if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: Tmdb.rUrl + '/movies',
+    type: 'POST',
+    data: { movie: {
+              XXXXXXXXXXXXXXXXXXXXXXXXX,
+         }
+      }
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+};
