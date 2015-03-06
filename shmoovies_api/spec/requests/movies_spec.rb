b4b95e6217e7c582e6b2b4f4f126af1cab17128a
J-Weeks <weeksjasons@gmail.com>
2015-03-06 11:54:03 -0500

    added and passed a show test for a movie (singular)

--- shmoovies_api/spec/requests/movies_spec.rb
+++ shmoovies_api/spec/requests/movies_spec.rb
@@ -27,8 +27,15 @@ describe 'Movie Requests' do
     end
   end
 
-#  describe '#show' do
-#    it 'get a post by '
+  describe '#show' do
+    it 'get a post by ID and return JSON data' do
+      @movie = @movies.first
+      get "/movies/#{@movie.id}"
+      expect(response).to be_success
+      movie = JSON.parse(response.body)
+      expect(movie['tmbd_id']).to eq @movie.tmbd_id
+    end
+  end
 
 
 
