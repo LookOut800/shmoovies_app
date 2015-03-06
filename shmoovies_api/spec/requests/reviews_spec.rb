ff4876c23110e0239a9a3f5f41146bd63f345789
J-Weeks <weeksjasons@gmail.com>
2015-03-06 14:34:00 -0500

    added factorygirl to and create reviews spec as well as passing index test

--- shmoovies_api/spec/requests/reviews_spec.rb
+++ shmoovies_api/spec/requests/reviews_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+describe 'Reviews Requests' do
+  before(:all) do
+    Movie.destroy_all
+    Review.destroy_all
+    @movies = FactoryGirl.create_list(:movie, 10)
+    @reviews = FactoryGirl.create_list(:review, 10)
+  end
+
+  describe '#index' do
+    it 'gets all reviews for a movie' do
+      get "/movies/#{@movies.first.id}/reviews"
+      expect(response).to be_success
+    end
+  end
+
+end
