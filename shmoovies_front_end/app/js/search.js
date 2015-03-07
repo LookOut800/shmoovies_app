56cfda7c3b00b71d2fe82e0a373b6fad68d14a86
J-Weeks <weeksjasons@gmail.com>
2015-03-07 15:52:59 -0500

    Started working on search logic, still in unfinished state

--- shmoovies_front_end/app/js/search.js
+++ shmoovies_front_end/app/js/search.js
@@ -1,3 +1,41 @@
-jQuery(document).ready(function($) {
+var Search = Search || {
+  tUrl: 'http://api.themoviedb.org/3/movie/',
+  key: '?api_key=c688559a6fc49e17efa91cf8b357837f',
+  rUrl: 'http://localhost:3000'
+};
+
+
+
+$(document).ready(function(){
+
+Search.getSearchString();
 
 });
+
+Search.getSearchString = function(){
+
+   $('forms.form-group').on('submit', function(event){
+
+   searchString = $('movie-query-input').val();
+
+    Search.compareSearch(searchString);
+
+  });
+};
+
+
+Search.compareSearch = function(event){
+  if (event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: Search.rUrl + '/movies',
+    type: 'GET',
+  }).done(function(data){
+    // console.log(data);
+    Search.moviesIndex(data);
+  })
+  .fail(function(jqXHR, textStatus, errorThrown){
+    console.log(jqXHR, textStatus, errorThrown);
+  });  
+
+}
+
