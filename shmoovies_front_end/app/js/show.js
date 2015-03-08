620a63fd8338952596d8cd74ee476240d25372b5
Max Blaushild <blaushmild@gmail.com>
2015-03-08 16:00:49 -0400

    adds ajax and handlebars to render all reviews for a movie

--- shmoovies_front_end/app/js/show.js
+++ shmoovies_front_end/app/js/show.js
@@ -4,11 +4,14 @@ var Tmdb = Tmdb || {
   rUrl: 'http://localhost:3000'
 };
 
-var locate = window.location.search
-locate = locate.substring(1, locate.length);
+
 
 $(document).ready(function(){
+  var locate = window.location.search
+  locate = locate.substring(1, locate.length);
+
   Tmdb.getMovieShow(locate);
+  Tmdb.getReviews(locate);
 });
 
 Tmdb.getMovieShow = function(num){
@@ -21,6 +24,17 @@ Tmdb.getMovieShow = function(num){
   });
 };
 
+Tmdb.getReviews = function(num){
+  $.ajax({
+    url: Tmdb.rUrl + '/movies/' + num,
+    type: 'GET',
+    dataType: 'JSON'
+  }).done(Tmdb.renderReviews)
+  .fail(function(jqXHR, textStatus, errorThrown){
+    console.log(jqXHR, textStatus, errorThrown);
+  });
+};
+
 Tmdb.renderShow = function(data){
   var template = Handlebars.compile($('#movie-info').html());
   $('#show-content').html(template({
@@ -28,19 +42,26 @@ Tmdb.renderShow = function(data){
   }));
 };
 
+Tmdb.renderReviews = function(data){
+  var template = Handlebars.compile($('#review-info').html());
+  $('#show-reviews').html(template({
+    movie: data[0]
+  }));
+};
 
-// Tmdb.submitMovie = function(){
-//   if(event.preventDefault) event.preventDefault();
-//   $.ajax({
-//     url: Tmdb.rUrl + '/movies',
-//     type: 'POST',
-//     data: { movie: {
-//               tmbd_id:,
-//          }
-//       }
-//   }).done(function(data){
-//     trace(data);
-//   }).fail(function(jqXHR, textStatus, errorThrown){
-//     trace(jqXHR, textStatus, errorThrown);
-//   });
-// };
+
+Tmdb.submitMovie = function(){
+  if(event.preventDefault) event.preventDefault();
+  $.ajax({
+    url: Tmdb.rUrl + '/movies',
+    type: 'POST',
+    data: { movie: {
+              tmbd_id: locate
+         }
+      }
+  }).done(function(data){
+    trace(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  });
+};
