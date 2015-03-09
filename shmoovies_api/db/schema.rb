5633f5b2531373ae2eb1476d0a0a606ef30fa572
Charles Talbot <charlietalbot@gmail.com>
2015-03-09 15:14:25 -0400

    createUsers migration

--- shmoovies_api/db/schema.rb
+++ shmoovies_api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305203704) do
+ActiveRecord::Schema.define(version: 20150309191138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20150305203704) do
   end
 
   add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   add_foreign_key "reviews", "movies"
 end
