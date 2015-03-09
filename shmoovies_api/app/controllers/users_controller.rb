41a6d329dea4f6e2771aba8fb1e6d1651d68f09b
Max Blaushild <blaushmild@gmail.com>
2015-03-09 15:23:27 -0400

    creates user controller

---
+++ shmoovies_api/app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+      # The following line forces authentication for all of the following actions
+      # You may want to limit that to specific actions with only: [:action_name_1, :action_name_2] as an argument
+      before_filter :authenticate, only: [:index]
+
+      def sign_in
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+          render json: { token: user.token }
+        else
+          head :unauthorized
+        end
+      end
+
+      def index
+        # This is probably dangerous without a serializer as it will return sensitive information
+        render json: User.all, status: :ok
+      end
+
+      def show
+        # This is probably dangerous without a serializer as it will return sensitive information
+        @user = User.find(params[:id])
+        render json: @user
+      end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: {token: @user.token}
+        else
+          render json: {message: 'failed', status: 500}
+        end
+      end
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :token)
+      end
+    end
